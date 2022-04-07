1b6e1ce08161c014b080b6ba334c74bd7674373c
Matheus Pires <matheushenriquepires99@gmail.com>
2022-04-07 10:38:56 -0300

    feat: create Resolvers and Queris from services

---
+++ classroom/src/services/enrollments.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+
+import { PrismaService } from '../database/prisma/prisma.service';
+
+type GetByCourseAndStudentIdParams = {
+  courseId: string;
+  studentId: string;
+};
+
+@Injectable()
+export class EnrollmentsService {
+  constructor(private prisma: PrismaService) {}
+
+  getAllEnrollments() {
+    return this.prisma.enrollment.findMany({
+      where: {
+        cancelledAt: null,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+
+  getEnrollmentByCourseAndStudentId({
+    courseId,
+    studentId,
+  }: GetByCourseAndStudentIdParams) {
+    return this.prisma.enrollment.findFirst({
+      where: {
+        courseId,
+        studentId,
+        cancelledAt: null,
+      },
+    });
+  }
+
+  getAllEnrollmentsByStudentId(studentId: string) {
+    return this.prisma.enrollment.findMany({
+      where: {
+        studentId,
+        cancelledAt: null,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+}
